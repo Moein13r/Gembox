e741e5876df8c45be34ced05b6c1f731eb44878a
Moein13r <91881449+Moein13r@users.noreply.github.com>
2021-10-04 13:46:49 +0330

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<meta charset="utf-8" >
+<title>payment</title>
+<style>
+.test{
+background-color:lightblue;	
+margin-inline:auto;
+}
+</style>
+</head>
+<body>
+<?php
+
+?>
+<div class="test">
+<P>hey this is a test</p>
+</div>
+</body>
+</html>
